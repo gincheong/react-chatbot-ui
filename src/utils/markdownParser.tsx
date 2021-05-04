357f1ea3e0d2263cbca0bf805b5ee427f7ab67b1
gincheong <gincheong2@gmail.com>
2021-05-04 11:36:31 +0900

    chore: remove whitespace

--- src/utils/markdownParser.tsx
+++ src/utils/markdownParser.tsx
@@ -5,7 +5,6 @@ export const markdownParser = (string: string) => {
   const underlineRegex = /[_]{2}(?=[^\s])([^(__)]+)[_]{2}/gm;
   const strikeRegex = /[~]{1}([^~]+)[~]{1}/gm;
 
-
   return string
     .replace(codeRegex, '<code>$1</code>')
     .replace(boldRegex, '<b>$1</b>')
