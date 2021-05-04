8cee00f043e9a1bee8195b8844728183a1117670
gincheong <gincheong2@gmail.com>
2021-05-04 16:41:53 +0900

    feat: markdown Link 문법 추가
    - 기존 https:// 링크 정규식으로 추출하던 거 마크다운 문법으로 변경
      ([링크 이름](주소) -> <url data-url="주소">링크 이름</url>)
    - strikethrough 문법 tilde 두 개 쓰게 변경(~~)
    - underline 정규식에 오타 수정 (</i> -> </u>)
    - MsgParser에서 새 창 여는 부분 새 문법에 맞게 수정
    
    close #7

--- src/utils/urlParser.tsx
+++
@@ -1,5 +0,0 @@
-export const urlParser = (string: string) => {
-  const regex = new RegExp('(https?:[//][0-9a-z?&=+#%@~./-|ㄱ-ㅎ|ㅏ-ㅣ|가-힣]+)', 'mi');
-
-  return string.replace(regex, '<url>$1</url>');
-};
