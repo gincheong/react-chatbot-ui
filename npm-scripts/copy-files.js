0481d2d7151571b4c42b0b29f3be971af4f744d8
gincheong <gincheong2@gmail.com>
2021-05-16 15:51:59 +0900

    chore: delete unused parameter

--- npm-scripts/copy-files.js
+++ npm-scripts/copy-files.js
@@ -3,7 +3,7 @@ const fse = require('fs-extra');
 const srcPath = './src/assets/';
 const destPath = './dist/assets/';
 
-const filterFunc = (src, dest) => {
+const filterFunc = (src) => {
   return /\.svg$|.\/$/.test(src); // 폴더와, 확장자가 svg인 파일만 복사
 };
 
