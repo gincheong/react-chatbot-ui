9b5501f43794f3ae521dc3b2345cde1772d8b2cc
gincheong <gincheong2@gmail.com>
2021-05-27 21:14:25 +0900

    fix: set modal width, height with viewport

--- src/shared/Modal.tsx
+++ src/shared/Modal.tsx
@@ -9,8 +9,8 @@ interface Props {
 const StyledModal = styled.div`
   position: fixed;
   background-color: rgba(100, 100, 100, 0.5);
-  width: 100%;
-  height: 100%;
+  width: 100vw;
+  height: 100vh;
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
